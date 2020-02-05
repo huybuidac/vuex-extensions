d081a7538d1f6560ac9091d4b961f5cb4a3c1336
Huy Bui Dac <huybd1@fsoft.com.vn>
2020-02-05 21:26:07 +0700

    update type

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,7 +3,9 @@ import { Store, StoreOptions, GetterTree, ActionTree, MutationTree } from 'vuex'
 export interface ResetModuleOption {
   self?: boolean
   nested?: boolean
-  modules?: ResetModuleOption[]
+  modules?: {
+    [key: string]: ResetModuleOption
+  }
 }
 
 export interface StoreExtended<S> extends Store<S> {
