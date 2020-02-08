a8b2b73323c2c35b215e212b796a67a6b4c692a0
Huy Bui Dac <huybd1@fsoft.com.vn>
2020-02-08 16:01:24 +0700

    add types doc

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,14 +1,39 @@
 import { Store, StoreOptions, GetterTree, ActionTree, MutationTree } from 'vuex'
 
+/**
+ * Configurable options for `store.reset`
+ */
 export interface ResetModuleOption {
+  /**
+   * Whether this module is resetted or not
+   * 
+   * If undefined, it equals `parent's nest` property
+   */
   self?: boolean
+  /**
+   * Whether all sub modules are resetted or not
+   * 
+   * If undefined, it will be assigned by `self`
+   */
   nested?: boolean
+  /**
+   * Detail configuration for all submodules
+   */
   modules?: {
     [key: string]: ResetModuleOption
   }
 }
 
+
 export interface StoreExtended<S> extends Store<S> {
+  /**
+   * Reset store
+   * @param options root module configuration
+   * 
+   * - Please see https://github.com/huybuidac/vuex-extensions for detail
+   * 
+   * - If undefined, entire store will be reset
+   */
   reset(options?: ResetModuleOption): void;
 }
 
